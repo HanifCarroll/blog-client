9a46a07180808a45deb17b1ee5e0802828c34618
Hanif Carroll <hanifcarroll@gmail.com>
2018-11-02 19:18:43 -0400

    Init.

---
+++ components/head.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Head = () => (
+  <div>
+    <meta charSet="UTF-8" />
+    <title>Blog</title>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+
+    <link rel="icon" href="/static/favicon.ico" />
+  </div>
+);
+
+export default Head;
---
+++ pages/new.js
@@ -0,0 +1,9 @@
+export default () => {
+  return (
+    <div>
+      <h1>New Blog</h1>
+      <input type="text" />
+      <textarea />
+    </div>
+  );
+};
